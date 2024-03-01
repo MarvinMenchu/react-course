3e7d8e76f0d296ed39bc420669fc88739b6578e4
Ing. Marvin Menchu <marvin.menchu@icloud.com>
2024-03-01 17:07:29 -0600

    SE AGREGO COMPONENT ORDERCART

--- src/Components/CheckoutSideMenu/index.jsx
+++ src/Components/CheckoutSideMenu/index.jsx
@@ -1,12 +1,12 @@
 import { useContext } from 'react'
 import { ShoppingCardContext } from '../../Context'
 import { XMarkIcon } from '@heroicons/react/20/solid'
+import OrderCard from '../OrderCard'
 import './style.css'
 
 const CheckoutSideMenu = () => {
   
-    const { isCheckoutSideMenu, closeCheckoutSideMenu } = useContext(ShoppingCardContext);
-
+  const { isCheckoutSideMenu, closeCheckoutSideMenu,  cartProducts} = useContext(ShoppingCardContext)
   return (
     <aside className={`${isCheckoutSideMenu ? 'flex' : 'hidden' } checkout-side-menu flex-col fixed right-0 border border-black rounded-lg bg-white`}>
       <div className="flex justify-between items-center p-6">
@@ -15,8 +15,17 @@ const CheckoutSideMenu = () => {
             <XMarkIcon className="h-6 w-6 text-black cursor-pointer"
             onClick={() => closeCheckoutSideMenu()} />
         </div>
-        
       </div>
+      <div className="px-6">
+            {cartProducts.map(product => (
+                <OrderCard
+                key={product.id}
+                title={product.title}
+                imageUrl={product.image}
+                price={product.price}
+                />
+            ))}
+        </div>
     </aside>
   )
 }
