f32ace3b2dacf5f881ed20f9b7a48fa49ca0ec1c
Ing. Marvin Menchu <marvin.menchu@icloud.com>
2024-03-04 17:20:24 -0600

    SE AGREGO PAGINA DE MY ORDERS Y COMPONENTE ORDERSCARD

--- src/Pages/MyOrder/index.jsx
+++ src/Pages/MyOrder/index.jsx
@@ -1,7 +1,10 @@
 import { useContext } from "react";
+import { Link } from "react-router-dom"; 
 import { ShoppingCardContext } from "../../Context";
 import OrderCard from "../../Components/OrderCard";
 import Layout from "../../Components/Layout";
+import { ChevronLeftIcon } from "@heroicons/react/20/solid";
+
 
 function MyOrder() {
 
@@ -9,7 +12,13 @@ function MyOrder() {
 
   return (
     <Layout>
-      My Order
+      <div className="flex items-center justify-center relative w-80 mb-6">
+                <Link to="/my-orders" className="absolute left-0">
+                <ChevronLeftIcon className="h-6 w-6 text-black cursor-pointer" />
+                </Link>
+            
+                <h1>MyOrders</h1>
+            </div>
       <div className="flex flex-col w-80">
             {order?.slice(-1)[0].products.map(product => (
                 <OrderCard
--- src/Pages/MyOrders/index.jsx
+++ src/Pages/MyOrders/index.jsx
@@ -1,11 +1,33 @@
 import Layout from "../../Components/Layout";
+import OrdersCard from "../../Components/OrdersCard";
+import { useContext } from "react";
+import { ShoppingCardContext } from "../../Context";
+import { Link } from "react-router-dom"; 
 
 function MyOrders() {
+
+    const context = useContext(ShoppingCardContext);
+
     return (
         <Layout>
-        <h1>My Orders</h1>
+            <div className="flex items-center justify-center relative w-80 ">
+            
+                <h1>My Orders</h1>
+            </div>
+            {
+                context.order.map((order, index) => {
+                    <Link key={index} to={`/my-order/${order.id}`}>
+                    <OrdersCard
+                        key={order.date}
+                        totalPrice={order.totalPrice}
+                        totalProduts={order.totalProduts} 
+                    />
+                    </Link>
+                })
+            }
+            <OrdersCard />
         </Layout>
-    );
+    )
 }
 
 export default MyOrders;
